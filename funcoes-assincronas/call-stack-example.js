b0ed84f2ed9b9ac5b73527777c5d6cc2713ec15f
LuizGarbini <garbasneto@gmail.com>
2024-12-11 00:18:28 -0300

    modulo de call stack finalizado

---
+++ funcoes-assincronas/call-stack-example.js
@@ -0,0 +1,20 @@
+// Aqui no caso o 1 e o 4 ele está executando de forma síncrona por isso eles são os primeiros a aparecerem
+console.log(1);
+
+// Aqui no caso Microtasks tem prioridade maior do que os temporizadores como o setTimeout() e promisses().
+queueMicrotask(() => {
+  console.log(2);
+});
+
+//
+setTimeout(() => {
+  console.log(3);
+}),
+  1000;
+
+console.log(4);
+
+// Quando a promisse é resolvida ela entra na call-stack como uma microtask.
+Promise.resolve(true).then(() => {
+  console.log(5);
+});
